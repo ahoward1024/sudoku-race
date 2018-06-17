1d8b0d0763c1c15ed0b2da5ed4fe5f5318ab713d
Alex Howard <ahoward1024@gmail.com>
2018-06-16 21:18:49 -0600

    Remove failing tests
    Took out the tests because they were empty and failing the build

--- frontend/src/__tests__/App-test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import App from '../App';
-import renderer from 'react-test-renderer';
-
-test('Test the full App function', () => {
-  const tree = renderer.create().component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
--- frontend/src/__tests__/Board-test.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import Board from '../Board';
-import renderer from 'react-test-renderer';
-
-test('Test the Board component', () => {
-});
