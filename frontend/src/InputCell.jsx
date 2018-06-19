9d7a028096945f04a05b7b1842bd590631d65f92
Alex Howard <ahoward1024@gmail.com>
2018-06-19 17:47:39 -0600

    Add more robust test to the InputCell component
    The InputCell component checks whether the specified input from the user
    is a number or not and prevents the input from passing into the <input>
    HTML component if it is not a number. This new test checks the validity
    of this function.

--- frontend/src/InputCell.jsx
+++ frontend/src/InputCell.jsx
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
 
 class InputCell extends Component {
-  static isNumber(event) {
-    const keycode = event.keyCode;
-    if (keycode > 31 && (keycode < 48 || keycode > 57)) {
+  constructor(props) {
+    super(props);
+    this.isNumber = this.isNumber.bind(this);
+  }
+
+  isNumber(event) {
+    this.keycode = event.keyCode;
+    if (this.keycode > 31 && (this.keycode < 48 || this.keycode > 57)) {
       event.preventDefault();
+
+      return false;
     }
+
+    return true;
   }
 
   render() {
--- frontend/src/__tests__/InputCell.test.jsx
+++ frontend/src/__tests__/InputCell.test.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Enzyme, {shallow, mount, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import InputCell from '../InputCell';
+import sinon from 'sinon';
+
+Enzyme.configure({'adapter': new Adapter()});
 
 test('Create an InputCell', () => {
   const tree = renderer.create(<InputCell/>).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test('Test if the sent keypress was not a number', () => {
+  // Test case for keycode 100: d should return false
+  const event = new KeyboardEvent('keydown', {'keyCode': 100}),
+        result = shallow(<InputCell/>).instance()
+                 .isNumber(event);
+  document.dispatchEvent(event);
+  expect(result).toBe(false);
+});
+
+test('Test if the sent keypress was a number', () => {
+  // Test case for keycode 50: 2 should return true
+  const event = new KeyboardEvent('keydown', {'keyCode': 50}),
+        result = shallow(<InputCell/>).instance()
+                 .isNumber(event);
+  document.dispatchEvent(event);
+  expect(result).toBe(true);
 });
