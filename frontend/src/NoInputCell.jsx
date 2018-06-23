1096e93999df455325ef447257466e5712ab1e7b
Aaron <AaronBatilo@gmail.com>
2018-06-23 08:24:17 -0600

    Make NoInputCell stateless
    If there's no user interaction required for a Component, you can define
    the component to simply be a pure function:
    https://reactjs.org/docs/components-and-props.html

--- frontend/src/NoInputCell.jsx
+++ frontend/src/NoInputCell.jsx
@@ -1,32 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class NoInputCell extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      'index': this.props.index,
-      'size': this.props.size,
-      'value': this.props.value
-    };
-  }
-
-  render() {
-    return (
-      <input
-        className="cell-input noinput"
-        maxLength="1"
-        value={this.state.value}
-        readOnly="true"
-      />
-    );
-  }
+function NoInputCell(props) {
+  return <input
+            className="cell-input noinput"
+            maxLength="1"
+            value={props.value}
+            readOnly="true"
+            />;
 }
 
-NoInputCell.propTypes = {
-  'index': PropTypes.string,
-  'size': PropTypes.string,
-  'value': PropTypes.string
-};
+NoInputCell.propTypes = {'value': PropTypes.string};
 
 export default NoInputCell;
