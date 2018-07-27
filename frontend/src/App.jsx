26b7ef22e2a06a9d6db9e64cc5d397ce520e0690
Alex Howard <ahoward1024@gmail.com>
2018-07-26 19:36:36 -0600

    Create scaling values based on window size
    The App component now generates a value to size the cells of the board
    based on the current size of the browser window.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,40 @@ import './App.css';
 import Board from './Board';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
+  updateDimensions() {
+    this.forceUpdate();
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
   render() {
+    const height = window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.body.clientHeight;
+    const width = window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+    const dimension = Math.min(width, height);
+    let cellSize = dimension;
+    const multi = dimension / 6 * 2;
+    cellSize -= multi;
+    cellSize /= 9;
+    // This value scales the text of the sudoku board
+    const textScale = 0.65;
+
     return (
       <div>
         <h1 id="heading" align="center">Sudoku Race</h1>
-        <Board url="https://sudokurace.io/game.create"/>
+        <div id="bdiv">
+          <Board url="https://sudokurace.io/game.create" cellSize={cellSize} textScale={textScale}/>
+        </div>
       </div>
     );
   }
