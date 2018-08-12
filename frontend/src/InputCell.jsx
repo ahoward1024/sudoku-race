16fad6f0b0eaa3690351edc11d9fc48bd1eba67d
Alex Howard <ahoward1024@gmail.com>
2018-08-12 11:28:44 -0600

    Send user input to the server
    The `InputCell` class now uses the `sendMoveToServer()` function to send
    user input to the server after the user has tried to fill in one of the
    cells with a validated input (the characters 1-9).
    
    Technically, a blank character is a valid input, because we need to
    update the state of the `InputCell` to be nothing after the user has
    deleted an invalid character. However, we do not want to send blank
    input to the server, so this is also taken into account.

--- frontend/src/InputCell.jsx
+++ frontend/src/InputCell.jsx
@@ -4,17 +4,45 @@ import PropTypes from 'prop-types';
 class InputCell extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      'id': this.props.id,
-      'value': this.props.value
-    };
+    this.state = {'value': this.props.value};
     this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.sendMoveToServer = this.sendMoveToServer.bind(this);
+  }
+
+  async sendMoveToServer(url, value) {
+    try {
+      const response = await fetch(url, {
+        'method': 'POST',
+        'header': {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        'body': JSON.stringify({
+          'id': this.props.gameid,
+          'move': {
+            'pos': this.props.index,
+            'char': value
+          }
+        })
+      });
+      const json = await response.json();
+      console.log(`${this.props.index}: ${value} | ${json.status}`);
+      if (json.status === 'INCOMPLETE' || json.status === 'COMPLETE') {
+        this.props.updateBoard(json.board);
+      }
+    } catch (error) {
+      console.log(`Error ${url}: ${error}`);
+    }
   }
 
   handleKeyPress(event) {
     const inputText = event.target.value;
     if (InputCell.isValidInput(inputText)) {
       this.setState({'value': inputText});
+      if (inputText !== '') {
+        const sendMoveUrl = `${this.props.url}/game.move`;
+        this.sendMoveToServer(sendMoveUrl, inputText);
+      }
     }
   }
 
@@ -34,8 +62,8 @@ class InputCell extends Component {
 
     return (
       <input
-        key={this.state.id}
-        id={this.state.id}
+        key={this.props.id}
+        id={this.props.id}
         style={style}
         className="cell-input"
         onChange={this.handleKeyPress}
@@ -47,8 +75,12 @@ class InputCell extends Component {
 
 InputCell.propTypes = {
   'cellSize': PropTypes.number.isRequired,
+  'gameid': PropTypes.number.isRequired,
   'id': PropTypes.string.isRequired,
+  'index': PropTypes.number.isRequired,
   'textScale': PropTypes.number.isRequired,
+  'updateBoard': PropTypes.func.isRequired,
+  'url': PropTypes.string.isRequired,
   'value': PropTypes.string.isRequired
 };
 
