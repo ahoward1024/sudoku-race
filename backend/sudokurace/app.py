29b19337b7187e630f8670ec6428ffc088623f3f
Aaron <AaronBatilo@gmail.com>
2018-10-03 18:18:48 -0600

    Correctly set cache headers for versioned assets
    By default, using the app.static method to create the static route would
    set all the items to have a Cache-Control of no-cache, which defeats the
    point of us trying to smartly cache items with unique version hashes.
    
    This commit will correctly set cache headers by being cheeky with the
    dynamic routing that's supported by Sanic.

--- backend/sudokurace/app.py
+++ backend/sudokurace/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.6
 from sanic import Sanic
-from sanic.response import html
+from sanic.response import file
 from sanic_cors import CORS
 
 app = Sanic(__name__)
@@ -9,15 +9,42 @@ app.config.from_pyfile('config.py')
 try:
     app.config.from_pyfile('instance/config.py')
 except IOError as e:  # pragma: no cover
-    ...
-
-app.static('/static', './static')
-app.static('/service-worker.js', 'static/service-worker.js')
+    pass
 
 
 @app.route('/')
+@app.route('/index.html')
 async def root(req):  # pragma: no cover
-    return html(open('./static/index.html').read(),
-                headers={'Cache-Control': 'no-cache'})
+    return await file('./index.html', headers={'Cache-Control': 'no-cache'})
+
+
+@app.route('/service-worker.js')
+async def service_worker(req):  # pragma: no cover
+    return await file('./service-worker.js',
+                      headers={'Cache-Control': 'no-store'})
+
+
+@app.route('/static/css/main.<unique_hash>.css')
+async def css(req, unique_hash):  # pragma: no cover
+    return await file(f'./static/css/main.{unique_hash}.css',
+                      headers={'Cache-Control': 'max-age=31556926'})
+
+
+@app.route('/static/css/main.<unique_hash>.css<maps>')
+async def css_map(req, unique_hash, maps):  # pragma: no cover
+    return await file(f'./static/css/main.{unique_hash}.css{maps}',
+                      headers={'Cache-Control': 'max-age=31556926'})
+
+
+@app.route('/static/js/main.<unique_hash>.js')
+async def js(req, unique_hash):  # pragma: no cover
+    return await file(f'./static/js/main.{unique_hash}.js',
+                      headers={'Cache-Control': 'max-age=31556926'})
+
+
+@app.route('/static/js/main.<unique_hash>.js<maps>')
+async def js_map(req, unique_hash, maps):  # pragma: no cover
+    return await file(f'./static/js/main.{unique_hash}.js{maps}',
+                      headers={'Cache-Control': 'max-age=31556926'})
 
 from . import views  # noqa
