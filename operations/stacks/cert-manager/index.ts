a4c5814de157c55b83e5d53042f4e6d2ad7a99c1
Aaron <AaronBatilo@gmail.com>
2020-12-12 17:57:56 -0700

    Install cert-manager

---
+++ operations/stacks/cert-manager/index.ts
@@ -0,0 +1,22 @@
+import * as k8s from "@pulumi/kubernetes";
+
+export default async (): Promise<void> => {
+  const certManagerNamespace = new k8s.core.v1.Namespace("cert-manager", {
+    metadata: {
+      name: "cert-manager",
+      labels: { "istio-injection": "enabled" },
+    },
+  });
+
+  const certManager = new k8s.helm.v3.Chart("certManager", {
+    namespace: "cert-manager",
+    fetchOpts: {
+      repo: "https://charts.jetstack.io",
+    },
+    chart: "cert-manager",
+    version: "v1.1.0",
+    values: {
+      installCRDs: true,
+    },
+  });
+};
