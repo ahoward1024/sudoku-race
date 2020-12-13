639c35c61c07c98c3fa636ce34a0a84493f51ac3
Aaron <AaronBatilo@gmail.com>
2020-12-12 17:43:25 -0700

    Move metrics-server namespace

--- operations/stacks/metrics-server/index.ts
+++ operations/stacks/metrics-server/index.ts
@@ -2,6 +2,7 @@ import * as k8s from "@pulumi/kubernetes";
 
 export default async (): Promise<void> => {
   const metricsServer = new k8s.helm.v3.Chart("metrics-server", {
+    namespace: "operations",
     fetchOpts: {
       repo: "https://charts.bitnami.com/bitnami",
     },
