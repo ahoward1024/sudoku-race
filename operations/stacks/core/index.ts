103ede8313aa960974b2a1788de2ee9c6c58e189
Aaron <AaronBatilo@gmail.com>
2020-12-12 17:51:42 -0700

    Name namespace resource

--- operations/stacks/core/index.ts
+++ operations/stacks/core/index.ts
@@ -1,7 +1,7 @@
 import * as k8s from "@pulumi/kubernetes";
 
 export default async (): Promise<void> => {
-  const operationsNamespace = new k8s.core.v1.Namespace(name, {
+  const operationsNamespace = new k8s.core.v1.Namespace("operations", {
     metadata: {
       name: "operations",
       labels: { "istio-injection": "enabled" },
