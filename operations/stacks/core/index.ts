a18ef215ccd036d1652f9daeb9ef990f994f3586
Aaron <AaronBatilo@gmail.com>
2020-12-12 17:50:01 -0700

    Create operations namespace

---
+++ operations/stacks/core/index.ts
@@ -0,0 +1,10 @@
+import * as k8s from "@pulumi/kubernetes";
+
+export default async (): Promise<void> => {
+  const operationsNamespace = new k8s.core.v1.Namespace(name, {
+    metadata: {
+      name: "operations",
+      labels: { "istio-injection": "enabled" },
+    },
+  });
+};
