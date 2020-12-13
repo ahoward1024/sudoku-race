06ece1ffd5a2dee887f53cb916e75e27a991ce27
Aaron <AaronBatilo@gmail.com>
2020-12-12 21:38:05 -0700

    Add slow endpoint

--- sudokurace/entrypoint.py
+++ sudokurace/entrypoint.py
@@ -1,6 +1,7 @@
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
+import asyncio
 import uvicorn
 
 
@@ -8,11 +9,17 @@ async def homepage(request):
     return JSONResponse({"hello": "world"})
 
 
+async def slow(request):
+    await asyncio.sleep(10)
+    return JSONResponse({"slow": "slow"})
+
+
 def main():
     app = Starlette(
         debug=True,
         routes=[
             Route("/", homepage),
+            Route("/slow", slow),
         ],
     )
     uvicorn.run(app, host="0.0.0.0", port=8000)
