c00eaf37f062dd6065bfc8a08bc86a37f4203204
Aaron <AaronBatilo@gmail.com>
2020-12-15 21:59:46 -0700

    Remove istio injection from cockroachdb

--- operations/stacks/cockroachdb/index.ts
+++ operations/stacks/cockroachdb/index.ts
@@ -1,6 +1,29 @@
+import * as docker from "@pulumi/docker";
 import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
 
 export default async (): Promise<void> => {
+  const config = new pulumi.Config();
+  const pat = config.requireSecret("pat");
+  const appName = "sudoku-race-migrations";
+
+  // Build and push docker image to GitHub Container Registry
+  const image = new docker.Image(appName, {
+    imageName: `ghcr.io/abatilo/${appName}`,
+    build: {
+      context: "../",
+      dockerfile: "../migrations/Dockerfile.migrations",
+      env: {
+        DOCKER_BUILDKIT: "1",
+      },
+    },
+    registry: {
+      server: "ghcr.io/abatilo",
+      username: "abatilo",
+      password: pat,
+    },
+  });
+
   const cockroachdb = new k8s.helm.v3.Chart("cockroachdb", {
     namespace: "applications",
     fetchOpts: {
@@ -14,8 +37,16 @@ export default async (): Promise<void> => {
           size: "2Gi",
         },
       },
+      init: {
+        annotations: {
+          "sidecar.istio.io/inject": "false",
+        },
+      },
       statefulset: {
         replicas: 2,
+        annotations: {
+          "sidecar.istio.io/inject": "false",
+        },
       },
     },
   });
